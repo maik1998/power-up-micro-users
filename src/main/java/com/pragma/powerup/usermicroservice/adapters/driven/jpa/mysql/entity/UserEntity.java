3e14a5234f09d1e6fef3cb492918e7a664b752a9
Michael <jeanmaik11@gmail.com>
2023-05-01 22:38:37 -0500

    Estructuración de la UserEntity

--- src/main/java/com/pragma/powerup/usermicroservice/adapters/driven/jpa/mysql/entity/UserEntity.java
+++ src/main/java/com/pragma/powerup/usermicroservice/adapters/driven/jpa/mysql/entity/UserEntity.java
@@ -1,16 +1,12 @@
 package com.pragma.powerup.usermicroservice.adapters.driven.jpa.mysql.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.Date;
 
 @Entity
 @Table(name = "user")
@@ -28,4 +24,28 @@ public class UserEntity {
     @ManyToOne
     @JoinColumn(name = "id_role")
     private RoleEntity roleEntity;
+
+    @Column(name = "nombre")
+    private String nombre;
+
+    @Column(name = "apellido")
+    private String apellido;
+
+    @Column(name = "numero_documento")
+    private String numeroDocumento;
+
+    @Column(name = "celular")
+    private String celular;
+
+    @Column(name = "fecha_nacimiento")
+    private Date fechaNacimiento;
+
+    @Column(name = "correo")
+    private String correo;
+
+    @Column(name = "clave")
+    private String clave;
+
+    //Terminar las clases relacionadas con la entidad
+
 }
